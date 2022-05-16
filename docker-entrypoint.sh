b71bbeb018e13dcda3912640b9a01125e958b772
makoto-kubodera <makoto-kubodera@linked-ideal.com>
2022-05-17 06:46:28 +0900

    https://github.com/toposoid/toposoid-deduction-admin-web/issues/23

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd /app/toposoid-deduction-admin-web/target/universal
-toposoid-deduction-admin-web-0.3-SNAPSHOT/bin/toposoid-deduction-admin-web
+toposoid-deduction-admin-web-0.3/bin/toposoid-deduction-admin-web
